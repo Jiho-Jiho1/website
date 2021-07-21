16434d900a7cd7f22faeb01f6150e23a3e307f90
Jiho-Jiho1 <83953688+Jiho-Jiho1@users.noreply.github.com>
2021-07-21 20:54:07 +0900

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,8 +1,8 @@
 document.getElementById("fun").onclick= function(){
     const num = Math.floor(Math.random() * 2);
-
+    console.log(num)
     if (num===0){
-        window.location.href="https://funactivatedbutcrashed.com"
+        window.location.replace("https://funactivatedbutcrashed.com")
     }
     if (num===1){
         window.location.href="http://weavesilk.com/"
