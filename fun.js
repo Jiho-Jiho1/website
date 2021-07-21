5d70e31b11dc39bc483dda2b828a50f993d199b9
Jiho-Jiho1 <83953688+Jiho-Jiho1@users.noreply.github.com>
2021-07-21 21:05:55 +0900

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,11 +1,26 @@
 document.getElementById("fun").onclick= function(){
-    const num = Math.floor(Math.random() * 2);
+    const num = Math.floor(Math.random() * 7);
     console.log(num)
     if (num===0){
         window.location.replace("https://funactivatedbutcrashed.com")
     }
     if (num===1){
         window.location.href="http://weavesilk.com/"
+    }
+    if (num===2){
+        window.location.replace("https://funactivatedbutcrashedAGAINUNLUCKY.com")
+    }
+    if (num===3){
+        window.location.replace("http://wwwwwwwww.jodi.org.")
+    }
+    if (num===4){
+        window.location.replace("https://www.pointerpointer.com/")
+    }
+    if (num===5){
+        window.location.replace("https://www.WEBSITECRASHED.com")
+    }
+    if (num===6){
+        window.location.replace("https://www.WEBSITECRASHEDagain.com")
     }
 
     
