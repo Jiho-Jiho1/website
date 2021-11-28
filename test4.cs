2fd2327fd3ea9f4ea991d4e5e6363698f9163158
Jiho-Jiho1 <83953688+Jiho-Jiho1@users.noreply.github.com>
2021-11-28 09:40:28 +0900

    gtt4swbsnyju ilo;6olikgferbnml;o7lyi,tmngdbfsvdasx

---
+++ test4.cs
@@ -0,0 +1,22 @@
+using Cheese;
+
+namespace questions
+{
+    class Program
+    {
+        static void Main()
+        {
+    Console.WriteLine("What is your name?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"Nice to meet you, {input} !");
+
+      Console.WriteLine("How old are you?");
+      string input1 = Console.ReadLine();
+      Console.WriteLine($"{input} ,you are {input1} years old!");
+
+    Console.WriteLine("Ok, bye!");
+      string input2 = Console.ReadLine();
+      Console.WriteLine($"{input} ,have a great day! Bye!");
+        }
+    }
+}
